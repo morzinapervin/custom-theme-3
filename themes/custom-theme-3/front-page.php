139e74a8f6227633dd9993b9088adf1d17a7ad70
Morzina Pervin <76468425+morzinapervin@users.noreply.github.com>
2024-10-11 17:56:29 +0600

    theme file is not ignored now

---
+++ themes/custom-theme-3/front-page.php
@@ -0,0 +1,11 @@
+<?php
+get_header();
+?>
+<main id="primary" class="site-main">
+	<section class="hm-hero hero-section">
+this is testing custom theme 3
+
+	</section>
+</main>
+<?php
+get_footer();
